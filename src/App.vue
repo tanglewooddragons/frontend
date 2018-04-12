3bbb06566c0d78130c85a529fba7d7f2f2104313
Hudson <protegegod@gmail.com>
2018-04-12 18:15:14 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template lang='pug'>
+	router-view
+</template>
+
+<script>
+export default {
+	name: 'App'
+}
+</script>
+
+<style lang='stylus'>
+@import 'rhazp/src/rhazp.styl'
+</style>
---
+++ src/components/logins/Login.vue
@@ -0,0 +1,29 @@
+<template lang='pug'>
+form.login-box
+	img(src="./twd.png")
+	.form-row
+		.form-control
+			label E-mail
+			input(type='text', placeholder='username@example.com')
+	.form-row
+		.form-control
+			label Password
+			input(type='password', placeholder='***********')
+	.form-row
+		.form-control
+			button.lg.primary.fill Login
+	.form-row
+		.links #[router-link(to='/register') Register] | #[router-link(to='/forgot') Recover password]
+</template>
+
+<script>
+</script>
+
+<style lang='stylus' scoped>
+@import '../../rhazp/src/variables.styl'
+
+.links
+	width 100%
+	text-align center
+	font-size $font-size-sm
+</style>
